1667b57c68b1fe23fae1db0610eaba95af3aedec
haknyan <hak.nyan@gmail.com>
2020-07-19 21:13:23 +0900

    commit

---
+++ Python/JtoP/2.string.py
@@ -0,0 +1,69 @@
+print("#문자열 만들기")
+a = "Hello, world"
+b = 'Python is fun'
+c = """Life is too short, You need python"""
+d = '''Life is too short, You need python'''
+print('a = "Hello, world"', 'output:', a)
+print("b = 'Python is fun'", 'output:', b)
+print('c = """Life is too short, You need python"""', 'output:', c)
+print("d = '''Life is too short, You need python'''", 'output:', d)
+
+food = "Python's favorite food is perl"
+print('''\nfood = "Python's favorite food is perl"''', 'output:', food)
+
+say = '"Python is very easy." he says.'
+print("""say = '"Python is very easy." he says.'""", 'output:', say)
+
+food = 'Python\'s favorite food is perl'
+say = "\"Python is very easy.\" he says."
+print("\nfood = 'Python\'s favorite food is perl'", 'output', food)
+print('say = "\"Python is very easy.\" he says."', 'output', say)
+
+multiline = "Life is too short\nYou need python"
+print('\nmultiline = "Life is too short\\nYou need python"')
+print('output:')
+print(multiline)
+
+multiline='''
+Life is too short
+You need python
+'''
+print("""multiline=
+'''
+Life is too short
+You need python
+'''""")
+print('output:')
+print(multiline)
+
+multiline="""
+Life is too short
+You need python
+"""
+print('''multiline="""
+Life is too short
+You need python
+"""''')
+print('output:')
+print(multiline)
+
+print('#문자열 연산')
+head = "Python"
+tail = " is fun!"
+print('\"head = "Python", tail = " is fun!"\"')
+print('head + tail =', head + tail)
+print('head * 2=', head * 2)
+print('''
+print("=" * 50)
+print("My Program")
+print("=" * 50)''')
+print('output:')
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+
+a = "Life is too short"
+print('\'a = "Life is too short"\'')
+print('len(a):', len(a))
+
+print('\n#Indexing & Slicing')
